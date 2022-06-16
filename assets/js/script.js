a18f68e9956a6a7cbe813411acd8d807344ff340
Felipe Rodrigues Galvão <felipe.rgalvao@hotmail.com>
2022-06-16 12:59:33 -0300

    Terminando Projeto

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+    if (inputPergunta.value == "") {
+        alert("Digite sua pergunta")
+        return
+    }
+
+    const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+    // gerar numero aleatorio
+    const totalRespostas = respostas.length
+    const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+    elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+    elementoResposta.style.opacity = 1;
+
+    // sumir a resposta
+    setTimeout(function() {
+        elementoResposta.style.opacity = 0;
+    }, 3000)
+}
